e70ee7749e3150dbd5bec39f3bee76e7f72d087f
jangjh123 <gn000123@naver.com>
2023-02-09 15:15:40 +0900

    [D83T-9] Modifier 확장 함수 추가

--- presentation/src/main/java/com/d83t/bpm/presentation/compose/ComposeUtils.kt
+++ presentation/src/main/java/com/d83t/bpm/presentation/compose/ComposeUtils.kt
@@ -1,2 +1,25 @@
 package com.d83t.bpm.presentation.compose
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.input.pointer.pointerInput
+
+fun Modifier.addFocusCleaner(focusManager: FocusManager): Modifier {
+    return this.pointerInput(Unit) {
+        detectTapGestures {
+            focusManager.clearFocus()
+        }
+    }
+}
+
+fun Modifier.clickableWithoutRipple(onClick: () -> Unit): Modifier {
+    return this.clickable(
+        interactionSource = MutableInteractionSource(),
+        indication = null
+    ) {
+        onClick()
+    }
+}
