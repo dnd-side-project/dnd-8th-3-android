ea1c0da3e8860cd91cc88554a46a63868edc8052
jangjh123 <gn0001231@gmail.com>
2023-02-12 18:08:57 +0900

    컬러 테이블 추가

--- presentation/src/main/java/com/d83t/bpm/presentation/compose/theme/Color.kt
+++ presentation/src/main/java/com/d83t/bpm/presentation/compose/theme/Color.kt
@@ -3,5 +3,21 @@ package com.d83t.bpm.presentation.compose.theme
 import androidx.compose.ui.graphics.Color
 
 val Transparent = Color(0x00000000)
-val BackgroundColor = Color(0xFFFFFFFF)
-val MainTextColor = Color(0xFF000000)
+
+val MainBlackColor = Color(0xFF000000)
+val MainGreenColor = Color(0xFFA8FF0F)
+val PointRedColor = Color(0xFFFF2E00)
+
+val GrayColor0 = Color(0xFF171717)
+val GrayColor1 = Color(0xFF292929)
+val GrayColor2 = Color(0xFF3E3E3E)
+val GrayColor3 = Color(0xFF595959)
+val GrayColor4 = Color(0xFF838383)
+val GrayColor5 = Color(0xFF9EA0A4)
+val GrayColor6 = Color(0xFFBFBFBF)
+val GrayColor7 = Color(0xFFD1D1D3)
+val GrayColor8 = Color(0xFFE6E6E7)
+val GrayColor9 = Color(0xFFF0F0F1)
+val GrayColor10 = Color(0xFFF4F6F6)
+val GrayColor11 = Color(0xFFF5F6F6)
+val GrayColor12 = Color(0xFFFEFFFE)
