f24cbd24e7e4c6e82f25ed89c3dfac57c0f31350
jangjh123 <gn0001231@gmail.com>
2023-03-03 01:55:03 +0900

    [D83T-58] 누락된 import 추가

--- data/src/main/java/com/d83t/bpm/data/network/MainApi.kt
+++ data/src/main/java/com/d83t/bpm/data/network/MainApi.kt
@@ -13,6 +13,7 @@ import retrofit2.http.Headers
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Path
 
 interface MainApi {
     @Headers("shouldBeAuthorized: false")
